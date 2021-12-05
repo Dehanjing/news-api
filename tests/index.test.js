bda1c5453419c601efcf8c5e44836bb5f6f64ded
Dehanjing <Dehanjing@users.noreply.github.com>
2021-12-05 22:17:55 +0000

    Prettified Code | - M. Kendor Toge

--- tests/index.test.js
+++ tests/index.test.js
@@ -12,9 +12,13 @@ dapi._plugins.forEach((plugin_, index) => {
 function __scrapper(dapi, plugin) {
   dapi[plugin]((response, error) => {
     console.log(number++ + ". checking plugin -", plugin + ".js ---");
-    if (error) return console.error("error plugin " + plugin + "\n", error.stack);
+    if (error)
+      return console.error("error plugin " + plugin + "\n", error.stack);
 
     // passed error
-    return console.info("plugin " + plugin + " is tested out and not found any problem!", response.data.status);
+    return console.info(
+      "plugin " + plugin + " is tested out and not found any problem!",
+      response.data.status
+    );
   });
 }
