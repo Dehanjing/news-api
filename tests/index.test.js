1d02249ed997bfaaca46fbd6ae519785550496c5
Dehanjing <dehanapi@gmail.com>
2021-12-06 06:14:51 +0800

    Update tests/index.test.js

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,19 +1,20 @@
 let NewsApi = require("../src/");
 let dapi = new NewsApi();
+let number = 1;
 
-dapi._plugins.forEach((plugin_) => {
+dapi._plugins.forEach((plugin_, index) => {
   for (let plugin of Object.keys(plugin_)) {
     plugin = plugin.slice(0, -3);
-    dapi[plugin](function (response, error) {
-      console.log("checking plugin -", plugin + ".js");
-      if (error)
-        return console.error("error plugin " + plugin + "\n", error.stack);
-
-      // passed error
-      return console.info(
-        "plugin " + plugin + " is tested out and not found any problem!",
-        response.data.status
-      );
-    });
+    __scrapper(dapi, plugin);
   }
 });
+
+function __scrapper(dapi, plugin) {
+  dapi[plugin]((response, error) => {
+    console.log(number++ + ". checking plugin -", plugin + ".js ---");
+    if (error) return console.error("error plugin " + plugin + "\n", error.stack);
+
+    // passed error
+    return console.info("plugin " + plugin + " is tested out and not found any problem!", response.data.status);
+  });
+}
